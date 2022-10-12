807883d89f17a65727094b7fd61facd1864b32d5
devWeibel <weibel.jason@gmail.com>
2022-10-12 19:08:30 -0400

    finished writing questions

--- assets/index.js
+++ assets/index.js
@@ -3,33 +3,54 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type: 'input',
-        message: 'What will you name your Repository?',
+        message: 'Provide a repository name.',
         name: 'title',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'Description of the project.',
+        name: 'description',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'URL for the project.',
+        name: 'URL',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'Provide any installation instructions.',
+        name: 'installation instructions',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'Instructions for how to use the application.',
+        name: 'usage information',
     },
     {
         type: 'input',
-        message: '',
-        name: '',
+        message: 'What resources contributed to the project?',
+        name: 'contribution guidelines',
+    },
+    {
+        type: 'input',
+        message: 'How was the project tested?',
+        name: 'test instructions',
+    },
+    {
+        type: 'list',
+        message: 'What license is authorized?',
+        name: 'license',
+        choices: ['MIT', 'ISC', 'None']
+    },
+    {
+        type: 'input',
+        message: 'GitHub user name?',
+        name: 'gitHub user name',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address.',
+        name: 'email',
     }
 ]
 
